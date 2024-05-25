e04124391e54084d0641d5792b3082c898e7e945
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2024-05-25 19:59:26 +0900

    一発で決まる多数決（重複チェック）

---
+++ MajorityVote/MajorityVote_CPP/MajorityVote_CPP.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+/*一発で決まる多数決は何通り出来るか*/
+int main()
+{
+    const int MAXPEOPLE = 100;
+    int count = 0;
+
+    for (int rock = 0; rock <= MAXPEOPLE; rock++) { //グーだった場合
+        for (int scissors = 0; scissors <= MAXPEOPLE - rock; scissors++) {//チョキだった場合
+            int paper = MAXPEOPLE - rock - scissors;//パーだった場合
+
+            if (rock > scissors) {
+                if (rock != paper) {
+                    count++;
+                }
+            }
+            else if (rock < scissors) {
+                if (scissors != paper) {
+                    count++;
+                }
+            }
+            else {
+                if (rock < paper) {
+                    count++;
+                }
+            }
+        }
+        std::cout << count << std::endl;
+    }
+}
