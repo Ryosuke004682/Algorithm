e391a71ba8254ae27b96edd76586995e974d544d
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2024-05-27 23:57:17 +0900

    ビット演算子を用いたアルゴリズム

---
+++ BitOperator/BitOperator_CS/BitOperator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //2^nを求めるアルゴリズム
+            const int MAX_STATION_NUM = 29;
+
+
+            int startStation = 1, endStation = 17;
+
+            int stationDifference = Math.Abs(startStation - endStation);//間にある駅の数を求める
+
+            //1 << (n - 1) == 2^(n - 1)
+            int firstShiftResult = (1 << (stationDifference - 1));
+
+            //(1 << (MAX_STATION_NUM - n - 1)) == 2^(MAX_STATION_NUM - n - 1)
+            int secondShiftResult = (1 << (MAX_STATION_NUM - stationDifference - 1));
+
+            Console.WriteLine ((firstShiftResult + secondShiftResult) - 1 + "通り");
+
+            while (true) { }
+        }
+    }
+}
