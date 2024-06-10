81dbaf039dc3ed1d76a226fdebf967f6c19d7399
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2024-06-11 00:44:39 +0900

    7segmentディスプレイの、転倒している量で考えるアルゴリズム

---
+++ SevenSegmentDisplayAlgorithm/SevenSegmentDisplay_CPP/SevenSegmentDisplay_CPP.cpp
@@ -0,0 +1,29 @@
+﻿// SevenSegmentDisplay_CPP.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+// NumeralConversion_CPP.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+#include <iostream>
+#include <vector>
+
+const int N = 30;
+
+int check(int num) {
+    int light[] = { 6 , 2 , 5 , 5 , 4 , 5 , 6 , 3 , 7 , 6 };
+    return light[num / 10] + light[num % 10];
+}
+
+int main() {
+    int cnt = 0;
+    for (int h = 0; h < 24; h++) {
+        for (int m = 0; m < 60; m++) {
+            for (int s = 0; s < 60; s++) {
+                if (check(h) + check(m) + check(s) == N) {
+                    cnt++;
+                }
+            }
+        }
+    }
+
+    std::cout << cnt << std::endl;
+    return 0;
+}
---
+++ SevenSegmentDisplayAlgorithm/SevenSegmentDisplay_CS/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenSegmentDisplay_CS {
+	internal class Program {
+		const int N = 30;
+
+		static void Main(string[] args) {
+			int cnt = 0;
+			for (int h = 0; h < 24; h++) {
+				for (int m = 0; m < 60; m++) {
+					for (int s = 0; s < 60; s++) {
+						if (check(h) + check(m) + check(s) == N) {
+							cnt++;
+						}
+					}
+				}
+			}
+			Console.WriteLine(cnt);
+			while (true) ;
+		}
+
+		static int check(int num) {
+			int[] light = { 6, 2, 5, 5, 4, 5, 6, 3, 7, 6 };
+			return light[num / 10] + light[num % 10];
+		}
+	}
+}
